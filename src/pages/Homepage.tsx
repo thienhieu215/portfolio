e49e8d850baf5b3a7daa416f4bb9035b4b6414ce
thienhieu215 <thienhieu215@gmail.com>
2021-08-31 04:19:40 +0700

    CSS fixes

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,32 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import style from './Page.module.scss'
+import { useMediaQuery } from 'react-responsive'
 
 const Homepage = () => {
 
+  const [clicked, setClicked] = useState<boolean>(false);
+
+  const bigScreen = useMediaQuery({ query: '(min-width: 769px)' })
+  const mobileTablet = useMediaQuery({ query: '(max-width: 769px)' })
+
   return (
     <div className={style.homepage}>
-      <div className={style.menu}>
-        <h1 >
-          {/* <div style={{ position: 'relative', display: 'block', overflow: 'hidden' }}> */}
+      {bigScreen &&
+        <div className={style.menu}>
+          <h1>
             <a className={style.link} href='/about'>
-              <div className={`${style.hover} ${style.link} ${style.color} ${style.labelHello}`}></div>
+              <div className={`${style.hover} ${style.link} ${style.labelHello}`}></div>
             </a>
-          {/* </div> */}
-          {/* <div style={{ position: 'relative', display: 'block', overflow: 'hidden' }}> */}
             <a className={style.link} href='/work'>
-              <div className={`${style.hover} ${style.link} ${style.color} ${style.labeliam}`}></div>
+              <div className={`${style.hover} ${style.link} ${style.labeliam}`}></div>
             </a>
-          {/* </div> */}
-          {/* <div style={{ position: 'relative', display: 'block', overflow: 'hidden' }}> */}
             <a className={style.link} href='/contact'>
-              <div className={`${style.hover} ${style.link} ${style.color} ${style.labelName}`}></div>
+              <div className={`${style.hover} ${style.link} ${style.labelName}`}></div>
             </a>
-          {/* </div> */}
-        </h1>
-      </div>
+          </h1>
+        </div>
+      }
+      {mobileTablet &&
+        <div className={style.mobileTapArea} onClick={() => setClicked(!clicked)}>
+          <div className={style.menu}>
+            <h1>
+              <div className={`${style.mobileLabel} ${style.link}`} style={{ display: clicked ? 'none' : 'block' }}>Hello.</div>
+              <a href='/about'>
+                <div className={`${style.mobileLabelActive} ${style.link}`} style={{ display: clicked ? 'block' : 'none' }}>About</div>
+              </a>
+              <div className={`${style.mobileLabel} ${style.link}`} style={{ display: clicked ? 'none' : 'block' }}>I am</div>
+              <a href='/work'>
+                <div className={`${style.mobileLabelActive} ${style.link}`} style={{ display: clicked ? 'block' : 'none' }}>Work</div>
+              </a>
+              <div className={`${style.mobileLabel} ${style.link}`} style={{ display: clicked ? 'none' : 'block' }}>Hieu Ng</div>
+              <a href='/contact'>
+                <div className={`${style.mobileLabelActive} ${style.link}`} style={{ display: clicked ? 'block' : 'none' }}>Contact</div>
+              </a>
+            </h1>
+          </div>
+          <div className={style.mobileIndicator}>tap anywhere</div>
+        </div>
+      }
       <div className={style.bgphoto}>
       </div>
-    </div>
+    </div >
   )
 }
 
